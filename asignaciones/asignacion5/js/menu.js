7f305610b943237ff186e91b50e7d3bb57b765f2
Pablo <corvinmarcus090@gmail.com>
2019-08-19 16:23:47 -0400

    a

--- asignaciones/asignacion5/js/menu.js
+++ asignaciones/asignacion5/js/menu.js
@@ -1,3 +1,3 @@
 function MenuDesplegable() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+	document.getElementsByClassName("navigation")[0].classList.MenuDesplegable("responsive");
 }
